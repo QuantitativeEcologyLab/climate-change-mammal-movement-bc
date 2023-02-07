415301969dd91e1f5f3c530ea49d443b267bdb68
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-02-07 10:53:45 -0800

    download data for each hour in the dataset

--- analysis/dowload-climate-data.R
+++ analysis/dowload-climate-data.R
@@ -1,34 +1,46 @@
+library('ctmm')   # for movement data
 library('ecmwfr') # for data from EU Centre for Medium-Range Weather Forecasts
 library('dplyr')  # for data wrangling
 
-wf_set_key(user = '165099',
-           key = '5612de2d-a0f8-438e-883f-5e8160e1a336',
-           service = 'cds')
+# get info from copernicus website
+wf_set_key(user = '', key = '', service = 'cds')
 
 # find which dates are needed
-dates <- readRDS('data/tracking-data/full-dataset.rds') %>%
+times <- readRDS('data/tracking-data/full-dataset.rds') %>%
   pull(tel) %>%
-  purrr::map(\(.t) as.character(unique(as.Date(.t$timestamp)))) %>%
+  purrr::map(\(.t) format(.t$timestamp, format = '%Y-%m-%d %H:00') %>%
+               unique() %>%
+               as.character()) %>%
   unlist() %>%
   unique() %>%
   sort()
+
+head(times) # check format
+
+#' *NOTE:* if the download fails due to an internal server error or an error
+#'         similar to the following:
+#'         - `No encoding supplied: defaulting to UTF-8.`,
+#'         - `Error in if (private$status == "completed") { :`
+#'             `argument is of length zero`,
+#'         try again after a minute or two.
+
+#' if the download fails, run the following line to start from the failed time
+times <- times[times >= timestamp]
+times[1]
 
 # to create a new request:
 #' *1* copy API from Copernicus website
 #' *2* paste it and highlight it and use `Addins` > `MARS to list`
 #' *3* edit as needed to run a test download or download all data  
-for(day in dates) {
-  message(paste0('Downloading ', day, '...'))
+for(timestamp in times) {
+  cat(paste0('Downloading ', timestamp, ' at ', Sys.time(), '...\n'))
   request <-
     list(
       variable = c('2m_temperature', 'snow_depth', 'total_precipitation'),
-      year = lubridate::year(day),
-      month = lubridate::month(day),
-      day = lubridate::day(day),
-      time = c('00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
-               '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
-               '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-               '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'),
+      year = format(as.POSIXct(timestamp), '%Y'),
+      month = format(as.POSIXct(timestamp), '%m'),
+      day = format(as.POSIXct(timestamp), '%d'),
+      time = paste0(format(as.POSIXct(timestamp), '%H'), ':00'),
       area = c(54, -121, 47, -113),
       format = 'netcdf.zip',
       dataset_short_name = 'reanalysis-era5-land',
@@ -40,5 +52,7 @@ for(day in dates) {
   unzip(zipfile = 'data/ecmwfr-data/download.netcdf.zip',
         exdir = 'data/ecmwfr-data')
   file.rename(from = 'data/ecmwfr-data/data.nc',
-              to = paste0('data/ecmwfr-data/', day, '-data.nc'))
+              to = paste0('data/ecmwfr-data/',
+                          format(as.POSIXct(timestamp), format = '%Y-%m-%d-%H'),
+                          '-data.nc'))
 }
