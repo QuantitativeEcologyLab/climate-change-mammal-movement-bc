9ced3d956ec92d793aa2d29ceb6862271efd64f2
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-03-27 11:08:56 -0700

    changed to relative to 2020

--- analysis/figures/habitat-quality-in-bc-parks.R
+++ analysis/figures/habitat-quality-in-bc-parks.R
@@ -23,21 +23,22 @@ parks <- read_sf('data/bc-parks-shapefile/TA_PEP_SVW_polygon.shp') %>%
 plot(bc_shp)
 plot(parks, col = '#228b2250', border = 'forestgreen', add = TRUE)
 
+# habitat quality within BC parks
 d <- tibble(species = c('Oreamnos_americanus', 'Puma_concolor',
                         'Rangifer_tarandus', 'Ursus_arctos_horribilis',
                         'Cervus elaphus'),
-            hq = map(c('data/habitat-quality-oreamnos-americanus.rds',
-                       'data/habitat-quality-puma-concolor.rds',
-                       'data/habitat-quality-rangifer-tarandus.rds',
-                       'data/habitat-quality-ursus-arctos-horribilis.rds',
-                       'data/habitat-quality-cervus-elaphus.rds'),
+            hq = map(c('data/habitat-quality-change-oreamnos-americanus.rds',
+                       'data/habitat-quality-change-puma-concolor.rds',
+                       'data/habitat-quality-change-rangifer-tarandus.rds',
+                       'data/habitat-quality-change-ursus-arctos-horribilis.rds',
+                       'data/habitat-quality-change-cervus-elaphus.rds'),
                      \(.fn) select(readRDS(.fn), scenario, x, y, mu) %>%
                        pivot_wider(names_from = scenario, values_from = mu)),
-            `2020` = map(hq, \(.d) .d %>%
-                           select(x, y, `2020`) %>%
-                           rast() %>%
-                           mask(parks) %>%
-                           as.data.frame()),
+            # `2020` = map(hq, \(.d) .d %>%
+            #                select(x, y, `2020`) %>%
+            #                rast() %>%
+            #                mask(parks) %>%
+            #                as.data.frame()),
             `2100 SSP 1-2.6` = map(hq, \(.d) .d %>%
                                      select(x, y, `2100 SSP 1-2.6`) %>%
                                      rast() %>%
@@ -59,21 +60,51 @@ d <- tibble(species = c('Oreamnos_americanus', 'Puma_concolor',
                                      mask(parks) %>% 
                                      as.data.frame())) %>%
   select(-hq) %>% # drop habitat quality tibble
-  unnest(c(`2020`:`2100 SSP 5-8.5`)) %>%
+  unnest(c(`2100 SSP 1-2.6`:`2100 SSP 5-8.5`)) %>%
   pivot_longer(- species, values_to = 'mu', names_to = 'scenario') %>%
   filter(! is.na(mu)) %>%
-  mutate(species = stringr::str_replace_all(species, '_', ' '))
+  mutate(species =
+           case_when(species == 'Oreamnos_americanus' ~ 'Mountain goat',
+                     species == 'Puma_concolor' ~ 'Cougar',
+                     species == 'Rangifer_tarandus' ~ 'Caribou',
+                     species == 'Ursus_arctos_horribilis' ~ 'Grizzly bear',
+                     species == 'Cervus elaphus' ~ 'Elk'))
 
-ggplot(d) +
-  facet_wrap(. ~ species, scales = 'free_y') +
-  geom_density(aes(mu, color = scenario), adjust = 2, linewidth = 1) +
-  scale_x_continuous(expression(bold(Habitat~quality~(log[2]))),
-                     limits = c(0.25, 4), trans = 'log2') +
-  ylab('Density') +
+d %>%
+  mutate(mu = if_else(mu > 4, 4, mu),
+         mu = if_else(mu < 0.25, 0.25, mu)) %>%
+  ggplot() +
+  facet_wrap(. ~ species, scales = 'fixed') +
+  geom_vline(xintercept = 1, color = 'grey') +
+  geom_density(aes(mu, color = scenario, fill = scenario), alpha = 0.1,
+               linewidth = 1, na.rm = TRUE, bw = 0.25) +
+  scale_x_continuous(
+    expression(bold(Change~'in'~habitat~quality~within~BC~parks~(log[2]))),
+    limits = c(0.25, 4), trans = 'log2') +
+  scale_y_continuous('Density') +
   scale_color_manual(
-    'Scenario', values = c('black', '#0571b0', '#92c5de', '#f4a582', '#ca0020'),
-    aesthetics = c('color', 'fill')) +
+    name = 'Scenario', aesthetics = c('color', 'fill'),
+    values = c('black', '#0571b0', '#92c5de', '#f4a582', '#ca0020')[-1]) +
   theme(legend.position = c(0.85, 0.2))
 
-ggsave('figures/habitat-quality-densities.png',
-       width = 10, height = 5, dpi = 600, bg = 'transparent', scale = 0.75)
+# d %>%
+#   group_by(species, scenario) %>%
+#   summarise(good = mean(mu >= 1),
+#             .groups = 'drop') %>%
+#   group_by(species) %>%
+#   mutate(good = good / first(good)) %>%
+#   ggplot() +
+#   facet_wrap(. ~ species, scales = 'fixed') +
+#   geom_hline(yintercept = 1, color = 'grey') +
+#   geom_segment(aes(scenario, xend = scenario, y = 1, yend = good,
+#                    color = scenario)) +
+#   geom_point(aes(scenario, y = good, color = scenario, fill = scenario)) +
+#   scale_x_discrete(NULL, breaks = NULL) +
+#   scale_y_continuous('Relative change in good habitat') +
+#   scale_color_manual(
+#     name = 'Scenario', aesthetics = c('color', 'fill'),
+#     values = c('black', '#0571b0', '#92c5de', '#f4a582', '#ca0020')) +
+#     theme(legend.position = c(0.85, 0.2))
+
+ggsave('figures/habitat-quality-densities-within-parks.png',
+       width = 10, height = 5, dpi = 600, bg = 'transparent')
